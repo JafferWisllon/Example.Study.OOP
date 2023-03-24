fb200c219785658180a0e6acb8e4ffde1344e4d0
Jaffer Wisllon <contatojaffer@gmail.com>
2023-03-23 22:31:57 -0300

    feat: class x object

---
+++ Example.Study.OOP.Classe/Produto.cs
@@ -0,0 +1,23 @@
+﻿namespace Example.Study.OOP.Classe
+{
+    public class Produto
+    {
+        private int Id;
+        public string Descricao {  get; set; }
+
+        public void ImprimirDescricao()
+        {
+            Console.WriteLine($"{Id} - {Descricao}");
+        }
+
+        public void SetId(int id)
+        {
+            Id = id;
+        }
+
+        public int GetId()
+        {
+            return Id;
+        }
+    }
+}
---
+++ Example.Study.OOP.Objeto/Program.cs
@@ -0,0 +1,16 @@
+﻿using Example.Study.OOP.Classe;
+
+namespace Example.Study.OOP.Objeto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var produto = new Produto();
+            produto.Descricao = "Teclado";
+            produto.SetId(10);
+
+            produto.ImprimirDescricao();
+        }
+    }
+}
